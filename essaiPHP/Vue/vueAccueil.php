eb78fc4a1c0293de1c7336d578be091adcc2de98
Unknown <luca.rosso30@gmail.com>
2018-07-26 11:14:10 +0200

    UML V3

---
+++ essaiPHP/Vue/vueAccueil.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="public/style/style.css">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="all">
+
+
+      <div class="cotegauche">
+        <div class="miseformg">
+          <textarea name="champ" rows="8" cols="80"></textarea>
+          <input type="button" name="envoie" value="envoyer">
+        </div>
+      </div>
+
+      <div class="cotedroit">
+        <div class="lettre">
+          <h3>Stat lettres</h3>
+        </div>
+        <div class="mot">
+          <h3>Stat mots</h3>
+        </div>
+        <div class="phrase">
+          <h3>Stat phrases</h3>
+        </div>
+      </div>
+
+    </div>
+  </body>
+</html>
