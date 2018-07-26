2f11503c81a7b15b53bd493e4be2d581bf647a85
Unknown <romain.fuentes@outlook.fr>
2018-07-26 10:34:38 +0200

    maj

--- essaiPHP/Vue/vueAccueil.php
+++ essaiPHP/Vue/vueAccueil.php
@@ -6,6 +6,7 @@
     <title></title>
   </head>
   <body>
+
     <div class="all">
 
 
@@ -14,11 +15,18 @@
           <textarea name="champ" rows="8" cols="80"></textarea>
           <input type="button" name="envoie" value="envoyer">
         </div>
-
       </div>
 
       <div class="cotedroit">
-
+        <div class="lettre">
+          <h3>Stat lettres</h3>
+        </div>
+        <div class="mot">
+          <h3>Stat mots</h3>
+        </div>
+        <div class="phrase">
+          <h3>Stat phrases</h3>
+        </div>
       </div>
 
     </div>
